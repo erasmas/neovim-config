26325d870a935fa8d6239670051699b3ec917837
Dmytro Morozov <mail@dmorozov.info>
2023-12-03 22:16:47 +0200

    Fix colorscheme for Zellij

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,16 +1,17 @@
 local M = {
   "folke/tokyonight.nvim",
-  commit = "e52c41314e83232840d6970e6b072f9fba242eb9",
+  commit = "6330395",
   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 M.name = "tokyonight-night"
 function M.config()
-  local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
-  if not status_ok then
-    return
-  end
+  -- https://github.com/zellij-org/zellij/issues/2444#issuecomment-1575314527
+  vim.schedule(function()
+    vim.cmd.colorscheme("tokyonight")
+    vim.api.nvim_set_option("background", "dark")
+  end)
 end
 
 return M
